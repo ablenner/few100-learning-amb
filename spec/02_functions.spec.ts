e834158038642e820fdbd6205e13085f8a8cc977
ablenner <54544412+ablenner@users.noreply.github.com>
2019-08-28 16:27:47 +0000

    morning tests

--- spec/02_functions.spec.ts
+++ spec/02_functions.spec.ts
@@ -53,8 +53,118 @@ describe('writing functions', () => {
                 const doubledEvens = numbers
                     .filter(isEven)
                     .map(n => n * 2);
+                const stringNumbers = numbers.map(n => n.toString());
                 expect(doubledEvens).toEqual([4, 8, 12, 16]);
             });
+
+            describe('some things about parameters to functions', () => {
+                it('cannot overload', () => {
+                    function formatName(first: string, last: string, mi?: string) {
+                        let fullName = `${last}, ${first}`;
+                        if (mi) {
+                            fullName += ` ${mi}.`;
+                        }
+                        return fullName;
+                    }
+
+                    expect(formatName('Han', 'Solo')).toBe('Solo, Han');
+                    expect(formatName('Han', 'Solo', 'D')).toBe('Solo, Han D.');
+                });
+
+                it('default values and rest', () => {
+                    function add(a: number = 10, b: number = 5, ...rest: number[]) {
+                        const firstTwo = a + b;
+                        return rest.reduce((s, n) => s + n, firstTwo);
+                    }
+                    expect(add(2, 2)).toBe(4);
+                    expect(add(2)).toBe(7);
+                    expect(add()).toBe(15);
+                    expect(add(undefined, 7)).toBe(17); // if you pass undefined, it will use the default value
+                    expect(add(1, 2, 3, 4, 5, 6, 7, 8, 9)).toBe(45);
+                });
+            });
+
+            describe('methods that return a single (scalar) value', () => {
+                it('for checking membership of an array', () => {
+                    const hasSomeEvens = numbers.some(isEven);
+                    expect(hasSomeEvens).toBe(true);
+                    const allEven = numbers.every(isEven);
+                    expect(allEven).toBe(false);
+                });
+
+                it('has a reduce method', () => {
+                    const total = numbers.reduce((s, n) => s + n);
+                    expect(total).toBe(45);
+                    const total2 = numbers.reduce((s, n) => s + n, 100);
+                    expect(total2).toBe(145);
+                });
+            });
+        });
+    });
+
+    describe('quick practice', () => {
+        interface Vehicle { vin: string; make: string; year: number; mileage: number; }
+        const data: Vehicle[] = [
+            { vin: '3938983', make: 'Ford Explorer', year: 2013, mileage: 100_013 },
+            { vin: '378493739', make: 'Honda Pilot', year: 2019, mileage: 823 },
+            { vin: '3843978', make: 'Chevy Camaro', year: 1973, mileage: 200_232 }
+        ];
+
+        it('practice 1', () => {
+            const answer = data
+                .filter(x => x.mileage > 100_000)
+                .map(x => x.make);
+            expect(answer).toEqual(['Ford Explorer', 'Chevy Camaro']);
+        });
+    });
+
+    describe('redux for dummies', () => {
+        interface Action { type: string; }
+        class Increment implements Action {
+            readonly type = 'Increment';
+        }
+        class Decrement implements Action {
+            readonly type = 'Decrement';
+        }
+        class Reset implements Action {
+            readonly type = 'Reset';
+        }
+
+        it('demo', () => {
+            const historyOfActions: Action[] = [
+                new Increment(),
+                new Increment(),
+                new Increment(),
+                new Increment(),
+                new Decrement(),
+                new Decrement(),
+                new Reset(),
+                new Increment(),
+                new Increment()
+            ];
+            interface State {
+                count: number;
+            }
+            const initialState: State = {
+                count: 0
+            };
+
+            const finalAnswer = historyOfActions.reduce((s: State, n: Action) => {
+                switch (n.type) {
+                    case 'Increment': {
+                        return {
+                            count: s.count + 1
+                        };
+                    }
+                    case 'Decrement': {
+                        return { count: s.count - 1 };
+                    }
+                    case 'Reset': {
+                        return initialState;
+                    }
+                }
+            }, initialState);
+            expect(finalAnswer.count).toBe(2);
         });
     });
 });
